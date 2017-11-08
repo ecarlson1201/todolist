64d11e0bfa524e9faafaa46f36415827907d0f78
Eric <eric.paul.carlson@gmail.com>
2017-11-07 19:22:25 -0600

    fixed onclick delete function

--- js/addNewItem.js
+++ js/addNewItem.js
@@ -1,26 +1,26 @@
 
 
-function newElement(){
+function newElement() {
     var li = document.createElement("li")
     var valueInput = document.getElementById("myInput").value
     var t = document.createTextNode(valueInput)
     li.appendChild(t)
-    if(valueInput === "") {
-    alert("You Must Write Something")
+    if (valueInput === "") {
+        alert("You Must Write Something")
     }
-    else{
+    else {
         document.getElementById("myUl").appendChild(li)
     }
     document.getElementById("myInput").value = ""
+
     var close = document.createElement("button")
     var x = document.createTextNode("x")
     close.className = "close"
     close.appendChild(x)
     li.appendChild(close)
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
-        }
+
+    close.onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
     }
 }
