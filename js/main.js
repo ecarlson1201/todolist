f5a39ae32affae3ade7257a05f0602351625c820
Eric <eric.paul.carlson@gmail.com>
2017-11-12 11:56:28 -0600

    finished checked styling//started check mark functionality

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function newElement() {
     li.onclick = function () {
         var classNameString = li.className;
         var splitClassName = classNameString.split(' ');
-
+        var checkMark = document.createTextNode("+")
         var foundIt = false
 
         for (i = 0; i < splitClassName.length; i++) {
@@ -25,15 +25,18 @@ function newElement() {
             if (splitClassName[i] === "checked") {
                 splitClassName.splice(0, 1);
                 foundIt = true;
+                li.removeChild(li.childNodes[1,2])
                 break;
             }
         }
         if (!foundIt) {
             splitClassName.splice(0, 0, "checked");
+            li.appendChild(checkMark)
         }
         var returnClass = splitClassName.join(" ");
         li.className = returnClass;
     }
+
     var close = document.createElement("button")
     var x = document.createTextNode("x")
     close.className = "close"
